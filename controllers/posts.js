e161d87080d9465a8c3d7d0cd1d68b3ce46a2366
krishna0999 <krishnamuraree123@gmail.com>
2022-01-10 09:17:12 +0530

    Deploying backend server to heroku

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,5 +1,6 @@
 //All the logic for the routes will be written in this js file
 
+import mongoose from "mongoose";
 import PostMessage from "../Models/postModel.js";
 
 export const getPosts = async (req, res) => {
@@ -11,15 +12,63 @@ export const getPosts = async (req, res) => {
   }
 };
 
-export const createPost = (req, res) => {
+export const createPost = async (req, res) => {
   const body = req.body;
 
   const newPost = new PostMessage(body);
 
   try {
-    newPost.save();
+    await newPost.save();
     res.status(201).json(newPost);
   } catch (error) {
     res.status(409).json({ message: error.message });
   }
+};
+
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params;
+
+  const post = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).send("No post found");
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(
+    _id,
+    { ...post, _id },
+    {
+      new: true,
+    }
+  );
+
+  res.json(updatedPost);
+};
+
+export const deletePost = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send("No post found");
+
+  await PostMessage.findByIdAndRemove(id);
+
+  res.json({ message: "Post deleted!" });
+};
+
+export const likedPost = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send("No post found");
+
+  const post = await PostMessage.findById(id);
+  const updatedPost = await PostMessage.findByIdAndUpdate(
+    id,
+    { likesCount: post.likesCount + 1 },
+    {
+      new: true,
+    }
+  );
+
+  res.json(updatedPost);
 };
--- server.js
+++ server.js
@@ -3,21 +3,23 @@ import mongoose from "mongoose";
 import cors from "cors";
 import bodyParser from "body-parser";
 import postRoutes from "./routes/posts.js";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
-app.use(express.json());
+app.use(cors());
 app.use(bodyParser.json({ limit: "30mb", extended: "true" }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: "true" }));
-app.use(cors());
+app.use(express.json());
 
 const PORT = process.env.PORT || 9000;
 
-const connection_url =
-  "mongodb+srv://krishna:GcQ-eC9GKFaNAvt@cluster0.xeqoq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-
 mongoose
-  .connect(connection_url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("Connected to database"))
   .catch((err) => console.log(err));
 
